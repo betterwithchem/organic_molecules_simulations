0caa5354bf649453ff976d12f21e60a98e20600c
betterwithchem <m.paloni@gmail.com>
2023-08-11 16:47:43 +0100

    Added Gaussian fit of Dihedral Angle distributions

--- sample_project.py
+++ sample_project.py
@@ -120,7 +120,7 @@ for sys in project.systems:
                     height=1.5, temp=300, pace=500, hills_file='HILLS_dih{}'.format(iangle),
                     biasfactor=5, grid_min='-pi', grid_max='pi', grid_spacing=2.5*np.pi/180)
 
-    plumed.writePlumedFile("{}/plumed.dat".format(sys.path),md,colvar="COLVAR",printstride=500)
+    plumed.writePlumedFile("{}/plumed.dat".format(sys.path),md,colvar="COLVAR",printstride=50)
 
     
 
